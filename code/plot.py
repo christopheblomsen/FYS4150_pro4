problem 9 done, if we fix susceptiliblity we could use both method to determine an average

code/plot.py
@@ -2,6 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pyarma as pa
 import os
+from scipy.stats import linregress
 
 # from matplotlib.ticker import ScalarFormatter
 plt.rc('text', usetex=True)
@@ -79,7 +80,7 @@ beta = 1
 
 
 
-os.chdir(r"/home/rhuvy/Documents/FYS4150/Projects/FYS4150_pro4/code")
+os.chdir(r"/home/rhuvy/Documents/FYS4150/Projects/FYS4150_pro4/code/data_8")
 
 # Create the arrays for problem 4
 # epsilon = np.zeros(20)
@@ -229,7 +230,7 @@ temperature = pa.mat()
 temperature.load("temp_prob8.bin")
 
 
-taille = 10
+taille = 50
 
 temperature = np.array(temperature).reshape(taille)
 
@@ -260,17 +261,17 @@ for k in range(len(temperature)):
     data8_40.load(f"L40_prob8_{k}.bin")
     data8_40 = np.array(data8_40)
 
-    # data8_60 = pa.mat()
-    # data8_60.load(f"L60_prob8_{k}.bin")
-    # data8_60 = np.array(data8_60)
+    data8_60 = pa.mat()
+    data8_60.load(f"L60_prob8_{k}.bin")
+    data8_60 = np.array(data8_60)
 
-    # data8_80 = pa.mat()
-    # data8_80.load(f"L80_prob8_{k}.bin")
-    # data8_80 = np.array(data8_80)
+    data8_80 = pa.mat()
+    data8_80.load(f"L80_prob8_{k}.bin")
+    data8_80 = np.array(data8_80)
     
-    # data8_100 = pa.mat()
-    # data8_100.load(f"L100_prob8_{k}.bin")
-    # data8_100 = np.array(data8_100)
+    data8_100 = pa.mat()
+    data8_100.load(f"L100_prob8_{k}.bin")
+    data8_100 = np.array(data8_100)
 
     # Calculate the four quantities
     eps40[k] = compute_epsilon(data8_40[0,:], 40.)
@@ -278,56 +279,56 @@ for k in range(len(temperature)):
     cv40[k] = compute_heat_capacity(data8_40[0,:],  T, 40.)
     chi40[k] = compute_susceptibility(data8_40[1,:],  T, 40.)
 
-    # eps60[k] = compute_epsilon(data8_60[0,:], 60.)
-    # m60[k] = compute_m_abs(data8_60[1,:], 60.)
-    # cv60[k] = compute_heat_capacity(data8_60[0,:],  T, 60.)
-    # chi60[k] = compute_susceptibility(data8_60[1,:],  T, 60.)
+    eps60[k] = compute_epsilon(data8_60[0,:], 60.)
+    m60[k] = compute_m_abs(data8_60[1,:], 60.)
+    cv60[k] = compute_heat_capacity(data8_60[0,:],  T, 60.)
+    chi60[k] = compute_susceptibility(data8_60[1,:],  T, 60.)
 
-    # eps80[k] = compute_epsilon(data8_80[0,:], 80.)
-    # m80[k] = compute_m_abs(data8_80[1,:], 80.)
-    # cv80[k] = compute_heat_capacity(data8_80[0,:],  T, 80.)
-    # chi80[k] = compute_susceptibility(data8_80[1,:],  T, 80.)
+    eps80[k] = compute_epsilon(data8_80[0,:], 80.)
+    m80[k] = compute_m_abs(data8_80[1,:], 80.)
+    cv80[k] = compute_heat_capacity(data8_80[0,:],  T, 80.)
+    chi80[k] = compute_susceptibility(data8_80[1,:],  T, 80.)
 
-    # eps100[k] = compute_epsilon(data8_100[0,:], 100.)
-    # m100[k] = compute_m_abs(data8_100[1,:], 100.)
-    # cv100[k] = compute_heat_capacity(data8_100[0,:],  T, 100.)
-    # chi100[k] = compute_susceptibility(data8_100[1,:],  T, 100.)
+    eps100[k] = compute_epsilon(data8_100[0,:], 100.)
+    m100[k] = compute_m_abs(data8_100[1,:], 100.)
+    cv100[k] = compute_heat_capacity(data8_100[0,:],  T, 100.)
+    chi100[k] = compute_susceptibility(data8_100[1,:],  T, 100.)
 
-import sys
-np.set_printoptions(threshold=sys.maxsize)
+# import sys
+# np.set_printoptions(threshold=sys.maxsize)
 
-print(data8_40[1,:])
+# print(data8_40[0,:])
 plt.figure()
-# plt.plot(temperature, cv100, 'k', label="100")
-# plt.plot(temperature, cv80, 'o', label="80")
-# plt.plot(temperature, cv60, 'b', label="60")
+plt.plot(temperature, cv100, 'k', label="100")
+plt.plot(temperature, cv80, 'o', label="80")
+plt.plot(temperature, cv60, 'b', label="60")
 plt.plot(temperature, cv40, 'g', label="40")
 plt.legend(loc='best')
 plt.xlabel("Temperature [J/k]")
 plt.ylabel(r"$C_v$")
 
 plt.figure()
-# plt.plot(temperature, chi100, 'k', label="100")
-# plt.plot(temperature, chi80, 'o', label="80")
-# plt.plot(temperature, chi60, 'b', label="60")
+plt.plot(temperature, chi100, 'k', label="100")
+plt.plot(temperature, chi80, 'o', label="80")
+plt.plot(temperature, chi60, 'b', label="60")
 plt.plot(temperature, chi40, 'g', label="40")
 plt.legend(loc='best')
 plt.xlabel("Temperature [J/k]")
 plt.ylabel(r"$\chi$")
 
 plt.figure()
-# plt.plot(temperature, m100, 'k', label="100")
-# plt.plot(temperature, m80, 'o', label="80")
-# plt.plot(temperature, m60, 'b', label="60")
+plt.plot(temperature, m100, 'k', label="100")
+plt.plot(temperature, m80, 'o', label="80")
+plt.plot(temperature, m60, 'b', label="60")
 plt.plot(temperature, m40, 'g', label="40")
 plt.legend(loc='best')
 plt.xlabel("Temperature [J/k]")
 plt.ylabel(r"$|m|$")
 
 plt.figure()
-# plt.plot(temperature, eps100, 'k', label="100")
-# plt.plot(temperature, eps80, 'o', label="80")
-# plt.plot(temperature, eps60, 'b', label="60")
+plt.plot(temperature, eps100, 'k', label="100")
+plt.plot(temperature, eps80, 'o', label="80")
+plt.plot(temperature, eps60, 'b', label="60")
 plt.plot(temperature, eps40, 'g', label="40")
 plt.legend(loc='best')
 plt.xlabel("Temperature [J/k]")
@@ -335,4 +336,29 @@ plt.ylabel(r"$\epsilon$")
 
 # print(temperature)
 
+# Problem 9 
+
+ind_40 = np.argmax(cv40)
+ind_60 = np.argmax(cv60)
+ind_80 = np.argmax(cv80)
+ind_100 = np.argmax(cv100)
+
+tc_40 = temperature[ind_40]
+tc_60 = temperature[ind_60]
+tc_80 = temperature[ind_80]
+tc_100 = temperature[ind_100]
+
+tc_arr = np.array([tc_40, tc_60, tc_80, tc_100])
+L_arr = np.array([40, 60, 80, 100])
+
+res = linregress(L_arr, tc_arr)
+print(res.intercept, res.slope)
+print(res.intercept_stderr)
+plt.figure()
+plt.plot(L_arr, tc_arr, 'ko')
+plt.plot(L_arr, res.intercept + res.slope*L_arr, 'r', label='fitted line')
+
+
 plt.show()
+
+